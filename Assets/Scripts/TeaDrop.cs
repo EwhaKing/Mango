6e93c1cacd2c87c60f246fa832867961e36f8f1e
win2dvp21 <win2dvp21@gmail.com>
2021-07-16 21:33:21 +0900

    작업 중

---
+++ Assets/Scripts/TeaDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeaDrop : MonoBehaviour
+{
+    int speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yMove = speed * Time.deltaTime;
+        this.transform.Translate(new Vector3(0, -yMove, 0));
+    }
+}
