d51ecdd6f4f00a3c02c059de6b05c85ea2b01ced
suabae <bsa0322@ewhain.net>
2021-07-20 00:58:15 +0900

    자몽 팔 애니메이션

--- Assets/Scripts/TeaManager.cs
+++ Assets/Scripts/TeaManager.cs
@@ -10,7 +10,7 @@ public class TeaManager : MonoBehaviour
     public Image[] fruits_image = new Image[INDEX]; //각각 반과일 이미지 UI
     public int[] fruits_num = new int[INDEX]; //각각 과일의 개수 저장하는 배열
     public int num = 6; //각 과일 개수의 합
-    public int finish_condition;
+    public int finish_condition; //종료조건 검사하기 위해서
     private bool first_fruit = true; //처음 뜨게 하기 위한 체크 변수
     private int fruit_current = 0; //현재 과일 인덱스
 
@@ -23,8 +23,10 @@ public class TeaManager : MonoBehaviour
     {
         left_arm.GetComponent<Animator>().enabled = false;
         right_arm.GetComponent<Animator>().enabled = false;
+        jamong_left_arm.GetComponent<Animator>().enabled = false;
+        jamong_right_arm.GetComponent<Animator>().enabled = false;
 
-        finish_condition = num;
+        finish_condition = num; //처음에 각 과일 개수 합으로 초기화 한 후, 0이 되면 속게임 종료
 
         for(int i = 0; i < INDEX; i++)
         {
@@ -95,13 +97,22 @@ public class TeaManager : MonoBehaviour
             }
          
         }
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //터치 시 팔 움직임
         {
             if(fruit_current == 1) //자몽이라면
             {
-                
+                if(jamong_left_arm.GetComponent<Animator>().isActiveAndEnabled == false)
+                {
+                    jamong_left_arm.GetComponent<Animator>().enabled = true;
+                    jamong_right_arm.GetComponent<Animator>().enabled = true;
+                }
+                else
+                {
+                    jamong_left_arm.GetComponent<Animator>().Play("arm_left_anim_jamong", -1, 0f);
+                    jamong_right_arm.GetComponent<Animator>().Play("arm_right_anim_jamong", -1, 0f);
+                }
             }
-            else
+            else //레몬, 꿀이라면
             {
                 if(left_arm.GetComponent<Animator>().isActiveAndEnabled == false)
                 {
@@ -114,7 +125,7 @@ public class TeaManager : MonoBehaviour
                     right_arm.GetComponent<Animator>().Play("arm_right_anim_lemon", -1, 0f);
                 }
             }
-            finish++;
+            finish++; //터치 판단
         }
 
         if (finish_condition == 0) fruits_image[0].gameObject.SetActive(true);
