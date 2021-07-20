c16c3f328a55d1320e75421eaf46ad97f6296c18
win2dvp21 <win2dvp21@gmail.com>
2021-07-20 12:03:37 +0900

    임시
    임시 작업

--- Assets/Scripts/BabyChange.cs
+++ Assets/Scripts/BabyChange.cs
@@ -2,18 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-<<<<<<< Updated upstream
 using UnityEngine.EventSystems;
-
-public class BabyChange : MonoBehaviour
-{
-    public Image Orig;
-    public Sprite New;
-    public Sprite New2;
-//    private float DelayTime = 1f;
-//    private float RealTime;
-
-=======
 
 public class BabyChange : MonoBehaviour
 {
@@ -22,40 +11,18 @@ public class BabyChange : MonoBehaviour
     public Image Orig; // 원본 거대 아기
     public Sprite New1; // 찡그린 거대 아기
     public Sprite New2; // 원본 거대 아기
->>>>>>> Stashed changes
 
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
         
     }
-    
-    
-=======
 
-    }
-
->>>>>>> Stashed changes
     // Update is called once per frame
 
     void Update()
     {
-<<<<<<< Updated upstream
-        if (Input.GetMouseButtonDown(0))
-        {
-            Handheld.Vibrate();
-            Orig.sprite = New;
-        }
 
-        else if (Input.GetMouseButtonUp(0))
-        {
-            /*RealTime += Time.deltaTime;
-            if (RealTime >= DelayTime)
-            {*/
-                Orig.sprite = New2;
-            //}
-=======
         // 클릭했을 때
         if (Input.GetMouseButtonDown(0))
         {
@@ -69,7 +36,6 @@ public class BabyChange : MonoBehaviour
         else if (Input.GetMouseButtonUp(0))
         {
             Orig.sprite = New2; // 원래 거대 아기로 이미지 변경
->>>>>>> Stashed changes
         }
 
     }
