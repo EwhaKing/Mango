cd382b1298c0cb0dcdc08677967e049eca22755d
suabae <bsa0322@ewhain.net>
2021-08-09 00:47:36 +0900

    엔딩,랭킹보드
    EndingButton 스크립트에는 다시시작, 게임종료 관련 코드가 있습니다. EndingManager에 닉네임 받을 때 확인버튼 관련 함수와, 어떤 엔딩을 띄워야할지 등에 대한 코드가 있습니다. 마지막으로 RankingManager에 랭킹보드에 대한 코드가 있습니다~!!

---
+++ Assets/Scripts/EndingButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndingButton : MonoBehaviour
+{
+    public void onRestartButton()
+    {
+        //static 변수 초기화
+        CustomerManager.check = false;
+        CustomerManager.current_customer = -1;
+        CustomerManager.customer_img_idx = new int[3];
+        CustomerManager.tip_money = new int[3];
+        CustomerManager.rich_check = 0;
+
+        TotalMoney.plusMoney = 0;
+        TotalMoney.totalMoney = 0;
+
+        RhythmBar.success_count = 0;
+        SliderTimer.time_over = false;
+        
+        //메인게임 씬 로드
+        SceneManager.LoadScene("maingameScene");
+    }
+
+    public void onQuitButton()
+    {
+        Application.Quit();
+    }
+
+}
