ff1a924bf696066b08c7f3d2ccd7484ed58d8ff9
win2dvp21 <win2dvp21@gmail.com>
2021-08-13 11:12:29 +0900

    시작 장면만

---
+++ Assets/Scripts/GameRule.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameRule : MonoBehaviour
+{
+
+    public GameObject ruleText;
+    //public GameObject ruleExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ruleText.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnClickGameRealRule()
+    {
+        ruleText.SetActive(true);
+    }
+
+    public void OnClickGameRealRuleExit()
+    {
+        ruleText.SetActive(false);
+    }
+}
