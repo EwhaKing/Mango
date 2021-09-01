97f0169d37ce0caeef7414c281c30620ea5a47bf
suabae <bsa0322@ewhain.net>
2021-09-02 05:33:49 +0900

    보관함 구현

--- Assets/Scripts/GameStaticData.cs
+++ Assets/Scripts/GameStaticData.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class GameStaticData : MonoBehaviour
 {
+    //데이터 베이스 저장 필요한 변수들
+    public int game_money = 0;
+    public List<int> baby_custom = new List<int>(2); //0: 옷정보, 1: 머리정보
+    public List<int> baby_clothes = new List<int>(5); //옷을 가지면 해당 인덱스에 1 저장, 아니면 0
 
-    public int game_money = 0; //데이터베이스 저장 필요
+
+    //데이터 베이스 저장 x 변수들
     public float clock_hand_rot = 0;
     public float mainscene_time = 0;
 
---
+++ Assets/Scripts/LockerItemButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LockerItemButton : MonoBehaviour
+{
+    public void onClickClothes()
+    {
+        int index = gameObject.transform.GetSiblingIndex(); //item 몇번째 자식인지
+
+        for(int i=0;i<gameObject.transform.parent.transform.childCount; i++)
+        {
+            if (i != index) gameObject.transform.parent.transform.GetChild(i).gameObject.transform.GetChild(0).gameObject.GetComponent<Image>().color = new Color(1f, 1f, 1f, 1f);
+        }
+        int clothe_sprite_num = GameObject.Find("GameManager").GetComponent<LockerManager>().locker_sprites[index];
+
+        GameObject.Find("GameData").GetComponent<GameStaticData>().baby_custom[0] = clothe_sprite_num; //0이 옷 정보
+        GameObject.Find("BabyCustom").GetComponent<BabyCustom>().changeBabyCustom(); //아기 옷 입히기 적용
+    }
+}
