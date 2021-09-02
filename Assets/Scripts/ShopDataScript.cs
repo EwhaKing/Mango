7d261c3da35e3b501d902fead2ae8642d61089a4
siyeonkm <68330823+siyeonkm@users.noreply.github.com>
2021-09-03 00:44:01 +0900

    Fix: "스프라이트 번호 추가"

--- Assets/Scripts/ShopDataScript.cs
+++ Assets/Scripts/ShopDataScript.cs
@@ -17,6 +17,7 @@ public class ItemData
     public string item_name;
     public int item_type;
     public int item_cost;
+    public int item_sprite;
     public bool own;
 
 }
@@ -28,7 +29,8 @@ public class ShopDataScript : MonoBehaviour
     {
         string str = File.ReadAllText(Application.dataPath + "/ShopData.json");
         sd = JsonUtility.FromJson<ShopData>("{\"item\":" + str + "}");
-        Debug.Log(str);
+
+        //확인용 출력
         for (int i = 0; i < sd.item.Length; i++)
         {
             Debug.Log("{NUM: " + sd.item[i].item_num + "  NAME: " + sd.item[i].item_name + "  TYPE: " + sd.item[i].item_type + "  COST: " + sd.item[i].item_cost + "  OWN: " + sd.item[i].own + "}");
@@ -44,6 +46,8 @@ public class ShopDataScript : MonoBehaviour
 
         string str2 = File.ReadAllText(Application.dataPath + "/ShopData.json");
         ShopData data_test = JsonUtility.FromJson<ShopData>("{\"item\":" + str2 + "}");
+
+        //확인용 출력
         Debug.Log("{NUM: " + sd.item[num].item_num + "  NAME: " + sd.item[num].item_name + "  TYPE: " + sd.item[num].item_type + "  COST: " + sd.item[num].item_cost + "  OWN: " + sd.item[num].own + "}");
 
     }
