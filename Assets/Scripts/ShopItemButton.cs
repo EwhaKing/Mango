7d8c324d8053d31d609d3dfce9d5e691cbbe9eab
suabae <bsa0322@ewhain.net>
2022-01-14 04:37:19 +0900

    상점 구매 오류 수정 + 보유중 뜨게

--- Assets/Scripts/ShopItemButton.cs
+++ Assets/Scripts/ShopItemButton.cs
@@ -9,8 +9,8 @@ public class ShopItemButton : MonoBehaviour
     public GameObject pay_popup; //구매 팝업창
     public GameObject pay_not; //구매 불가 팝업창
 
-    int clothe_sprite_num;
 
+    public static int current_buy; //현재 구매 버튼 누른 옷
 
     public void onClickClothes() //옷 버튼 클릭 시 옷 미리보기
     {
@@ -24,7 +24,7 @@ public class ShopItemButton : MonoBehaviour
     {
         int index = gameObject.transform.GetSiblingIndex(); //item 몇번째 자식인지
 
-        clothe_sprite_num = GameObject.Find("GameManager").GetComponent<ShopManager>().shop[index]; //고른 옷의 스프라이트 넘버
+        int clothe_sprite_num = GameObject.Find("GameManager").GetComponent<ShopManager>().shop[index]; //고른 옷의 스프라이트 넘버
 
         string want_buy_text = ShopDataScript.sd.item[clothe_sprite_num].item_name + " 을(를) 구매하시겠습니까?"; //옷 이름 구매 문구
         string cost_text = ShopDataScript.sd.item[clothe_sprite_num].item_cost.ToString(); //옷 가격
@@ -32,18 +32,19 @@ public class ShopItemButton : MonoBehaviour
         pay_popup.SetActive(true); //팝업창 띄우기
 
         //적용
-        pay_popup.transform.GetChild(0).gameObject.GetComponent<Text>().text = want_buy_text;
-        pay_popup.transform.GetChild(2).gameObject.GetComponent<Text>().text = cost_text;
+        pay_popup.transform.GetChild(1).gameObject.GetComponent<Text>().text = want_buy_text;
+        pay_popup.transform.GetChild(3).gameObject.GetComponent<Text>().text = cost_text;
 
+        current_buy = clothe_sprite_num;
     }
 
     public void onBuyButton() //구매버튼 클릭시
     {
         pay_popup.SetActive(false); //팝업창 닫기
 
-        int index = gameObject.transform.GetSiblingIndex(); //item 몇번째 자식인지
-        
-        int cost = ShopDataScript.sd.item[clothe_sprite_num].item_cost; //옷 돈
+        int cost = ShopDataScript.sd.item[current_buy].item_cost; //옷 돈
+
+        Debug.Log("옷 번호: " + current_buy);
 
         //돈이 부족하다면 -> 구매 불가 팝업
         if (GameStaticData.data.data_money - cost < 0)
@@ -55,17 +56,18 @@ public class ShopItemButton : MonoBehaviour
         {
             //구매 완료: 데이터베이스 소유 변경
             string str = File.ReadAllText(Application.dataPath + "/ShopData.json");
-            ShopDataScript.sd.item[clothe_sprite_num].own = true;
+            ShopDataScript.sd.item[current_buy].own = true;
 
             //돈 변경
             GameStaticData.data.data_money -= cost;
 
-            //상점 옷 삭제
-            Destroy(gameObject); //상점 진열 삭제
-            GameObject.Find("GameManager").GetComponent<ShopManager>().shop.RemoveAt(index); //상점 옷 스프라이트 삭제
+            //상점 옷 보유중 변경
+            gameObject.transform.parent.GetChild(current_buy).GetChild(1).transform.gameObject.SetActive(false); //가격버튼은 비활성화
+            gameObject.transform.parent.GetChild(current_buy).GetChild(2).transform.gameObject.SetActive(true); //보유중 버튼 활성화
+            gameObject.transform.parent.GetChild(current_buy).GetChild(0).gameObject.GetComponent<Button>().enabled = false; //옷 미리보기 비활성화
 
             //보관함에 옷 추가
-            GameObject.Find("GameManager").GetComponent<LockerManager>().addClotheLocker(clothe_sprite_num); //옷 보관함에 추가
+            GameObject.Find("GameManager").GetComponent<LockerManager>().addClotheLocker(current_buy); //옷 보관함에 추가
 
             //데이터베이스에 다시 저장
             File.WriteAllText(Application.dataPath + "/ShopData.json", JsonUtility.ToJson(ShopDataScript.sd));
