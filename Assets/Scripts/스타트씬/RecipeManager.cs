6f6aaa44b8030032698c40bfcd955d065873de07
win2dvp21 <84276596+win2dvp21@users.noreply.github.com>
2022-02-16 18:32:05 +0900

    레시피 팝업
    작업 중

---
+++ Assets/Scripts/스타트씬/RecipeManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeManager : MonoBehaviour
+{
+
+    public GameObject recipeDetailBg;
+
+    public Sprite DrinkBg;
+    public Sprite TeaBg;
+    public Sprite SpecialBg;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickDrink()
+    {
+        recipeDetailBg.GetComponent<Image>().sprite = DrinkBg;
+    }
+
+    public void OnClickTea()
+    {
+        recipeDetailBg.GetComponent<Image>().sprite = TeaBg;
+    }
+
+    public void OnClickSpecial()
+    {
+        recipeDetailBg.GetComponent<Image>().sprite = SpecialBg;
+    }
+}
