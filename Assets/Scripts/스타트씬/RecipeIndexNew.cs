2a8fef3745abacd65a7bf9daba266cb9c0eb4303
win2dvp21 <84276596+win2dvp21@users.noreply.github.com>
2022-03-11 21:35:16 +0900

    Update RecipeIndexNew.cs

--- Assets/Scripts/스타트씬/RecipeIndexNew.cs
+++ Assets/Scripts/스타트씬/RecipeIndexNew.cs
@@ -7,44 +7,7 @@ public class RecipeIndexNew : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject.Find("Recipe_list_title1").transform.GetChild(0).gameObject.SetActive(false);
-        GameObject.Find("Recipe_list_title2").transform.GetChild(0).gameObject.SetActive(false);
-        GameObject.Find("Recipe_list_title3").transform.GetChild(0).gameObject.SetActive(false);
-
-        int[] A = new int[] { 0, 5, 8, 11 };
-        int[] B = new int[] { 2, 3, 4, 6, 7, 9, 10, 12, 17 };
-        int[] C = new int[] { 1, 13, 14, 15, 16 };
-
-        for (int i = 0; i < 4; i++)
-        {
-            
-            if (TeaDataScript.teaDex.item[A[i]].is_read == true)
-            {
-                Debug.Log("티타입 0일 때 i는 " + A[i]);
-                GameObject.Find("Recipe_list_title1").transform.GetChild(0).gameObject.SetActive(true);
-            }
-        }
-
-        for (int j = 0; j < 9; j++)
-        {
-  
-            if (TeaDataScript.teaDex.item[B[j]].is_read == true)
-            {
-                Debug.Log("티타입 1일 때 j는 " + B[j]);
-                GameObject.Find("Recipe_list_title2").transform.GetChild(0).gameObject.SetActive(true);
-            }
-        }
-
-        for (int k = 0; k < 5; k++)
-        {
-            
-            if (TeaDataScript.teaDex.item[C[k]].is_read == true)
-            {
-                Debug.Log("티타입 2일 때 k는 " + C[k]);
-                GameObject.Find("Recipe_list_title3").transform.GetChild(0).gameObject.SetActive(true);
-            }
-        }
-
+        newIndex();
     }
 
     // Update is called once per frame
@@ -68,7 +31,6 @@ public class RecipeIndexNew : MonoBehaviour
 
             if (TeaDataScript.teaDex.item[A[i]].is_read == true)
             {
-                Debug.Log("티타입 0일 때 i는 " + A[i]);
                 GameObject.Find("Recipe_list_title1").transform.GetChild(0).gameObject.SetActive(true);
             }
         }
@@ -78,7 +40,6 @@ public class RecipeIndexNew : MonoBehaviour
 
             if (TeaDataScript.teaDex.item[B[j]].is_read == true)
             {
-                Debug.Log("티타입 1일 때 j는 " + B[j]);
                 GameObject.Find("Recipe_list_title2").transform.GetChild(0).gameObject.SetActive(true);
             }
         }
@@ -88,7 +49,6 @@ public class RecipeIndexNew : MonoBehaviour
 
             if (TeaDataScript.teaDex.item[C[k]].is_read == true)
             {
-                Debug.Log("티타입 2일 때 k는 " + C[k]);
                 GameObject.Find("Recipe_list_title3").transform.GetChild(0).gameObject.SetActive(true);
             }
         }
