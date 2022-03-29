d14a027d953556372d3eaeb9bb3e743bc2f0cca5
win2dvp21 <84276596+win2dvp21@users.noreply.github.com>
2022-03-30 00:00:46 +0900

    Update ByeBye.cs

--- Assets/Scripts/ByeBye.cs
+++ Assets/Scripts/ByeBye.cs
@@ -5,8 +5,7 @@ using UnityEngine.UI;
 
 public class ByeBye : MonoBehaviour
 {
-    int byeValue = 0;
-
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +17,8 @@ public class ByeBye : MonoBehaviour
     {
         if (Input.GetKeyUp(KeyCode.Escape))
         {
-            byeValue++;
-            if (!IsInvoking("disable_doubleClick"))
-            {
-                Invoke("disable_doubleClick", 0.5f);
-            }  
-        }
-
-        if (byeValue == 2)
-        {
-            CancelInvoke("disable_doubleClick");
             GameObject.Find("bye_popup_big").transform.GetChild(0).gameObject.SetActive(true);
-
-            byeValue = 0;
-        }
-    }
-
-    void disable_doubleClick()
-    {
-        byeValue = 0;
+        }   
     }
 
     public void realQuit()
@@ -45,8 +27,4 @@ public class ByeBye : MonoBehaviour
         Application.Quit();
     }
 
-    public void cancelQuit()
-    {
-        GameObject.Find("bye_popup_big").transform.GetChild(0).gameObject.SetActive(false);
-    }
 }
